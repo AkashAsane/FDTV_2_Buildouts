6394ba3130fc27f84f2c1dd5faa6d35b9fb28659
AkashAsane <akashasane4455@gmail.com>
2024-05-11 11:04:41 +0530

    test-commit

---
+++ Country_search/src/App.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from 'react';
+import axios from "axios";
+import './App.css';
+
+function  Countrysearch() {
+
+      const [data,setData]=useState([]);
+      const[query,setQuery]=useState('');
+      const[error,setError]=useState(null);
+
+      useEffect(()=>{
+        axios.get("https://restcountries.com/v3.1/all").then((response) => {
+          setData(response.data);
+          console.log(response.data);
+        })
+        .catch(error=>{
+          setError(error.messgage);
+          console.log("error",error);
+        })
+    
+      },[]);
+
+
+      const filteredCountries=data.filter((country)=>country.name.common.toLowerCase().startsWith(query.toLowerCase()))
+      console.log(filteredCountries);
+         
+      return(
+        <div className='main'>
+            <div className='navbar'>
+                <input
+                type='text'
+                placeholder='Search countries...'
+                onChange={(e)=>setQuery(e.target.value.toLowerCase())}
+                className='inputtxt'
+                />
+            </div>
+            <div className='countryWrapper'>
+                {   filteredCountries.length ===0 && query !=='' ?(
+                    <p>no countries found</p>
+                ): (
+                     filteredCountries.map(country=> (
+                        
+                            <div className='countryCard'>
+                              <img
+                                 src={country.flags.svg}
+                                 alt={country.name.common}
+                                 className='flag'
+                              />
+                              <p className='countryName'>{country.name.common}</p>
+                            </div>
+                     ))
+                )}
+            </div>
+        </div>
+      )
+}
+
+export default Countrysearch;
